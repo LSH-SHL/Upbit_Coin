bbaca179bacd1c431105ddf902d526bb9940ad70
LSH-SHL <60726651+LSH-SHL@users.noreply.github.com>
2022-01-08 07:42:37 +0900

    Update Load.py

--- Load.py
+++ Load.py
@@ -1,4 +1,5 @@
 import pyupbit
+#-*- coding: utf-8 -*-
 
 access = "fUxuI4HySxFEnpNklcUm3KE8raUq7WWxgZScqupF" 
 secret = "fy5TwwHtTA2GhLN46OKXyHfCsRBV8B2dsleHPZrf"
@@ -8,8 +9,21 @@ Contents = upbit.get_balances()
 total = float(Contents[0]['balance'])
 
 for i in range(1,len(Contents)):
-  print(Contents[i]['currency'] +' : ' + Contents[i]['balance'])
-  print('Avg Buy Price of ' +Contents[i]['currency']  + ' : '+ Contents[i]['avg_buy_price'] + Contents[i]['currency'])
+  # print(Contents[i]['currency'] +' : ' + Contents[i]['balance'])
+  # print('Avg Buy Price of ' +Contents[i]['currency']  + ' : '+ Contents[i]['avg_buy_price'] + Contents[i]['currency'])
   total += float(Contents[i]['balance']) * float(Contents[i]['avg_buy_price'])
 
-print(str(round(total)) + "Won")  
+print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+print("안녕하세요 이상현 님")
+
+
+print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+
+
+
+
+
+Property = format(round(total),',')
+
+print(str(Property) + "Won")  
+ 
