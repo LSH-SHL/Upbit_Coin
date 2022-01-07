b4f4443e5472dbb577addb228443486e1eddb898
LSH-SHL <60726651+LSH-SHL@users.noreply.github.com>
2022-01-08 07:35:48 +0900

    Update Load.py

--- Load.py
+++ Load.py
@@ -1,6 +1,15 @@
 import pyupbit
 
-access = "fUxuI4HySxFEnpNklcUm3KE8raUq7WWxgZScqupF" # 본인 값으로 변경 
-secret = "fy5TwwHtTA2GhLN46OKXyHfCsRBV8B2dsleHPZrf" # 본인 값으로 변경 
+access = "fUxuI4HySxFEnpNklcUm3KE8raUq7WWxgZScqupF" 
+secret = "fy5TwwHtTA2GhLN46OKXyHfCsRBV8B2dsleHPZrf"
 upbit = pyupbit.Upbit(access, secret)
-print(upbit.get_balances())
+
+Contents = upbit.get_balances()
+total = float(Contents[0]['balance'])
+
+for i in range(1,len(Contents)):
+  print(Contents[i]['currency'] +' : ' + Contents[i]['balance'])
+  print('Avg Buy Price of ' +Contents[i]['currency']  + ' : '+ Contents[i]['avg_buy_price'] + Contents[i]['currency'])
+  total += float(Contents[i]['balance']) * float(Contents[i]['avg_buy_price'])
+
+print(str(round(total)) + "Won")  
