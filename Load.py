22cc4512df815a04d0b94ebf1fb439f31cdfec6d
LSH-SHL <60726651+LSH-SHL@users.noreply.github.com>
2022-01-08 08:20:21 +0900

    Update Load.py

--- Load.py
+++ Load.py
@@ -6,24 +6,41 @@ secret = "fy5TwwHtTA2GhLN46OKXyHfCsRBV8B2dsleHPZrf"
 upbit = pyupbit.Upbit(access, secret)
 
 Contents = upbit.get_balances()
-total = float(Contents[0]['balance'])
+Purchase_Total = float(Contents[0]['balance'])
+Current_Total = float(Contents[0]['balance'])
 
 for i in range(1,len(Contents)):
   # print(Contents[i]['currency'] +' : ' + Contents[i]['balance'])
   # print('Avg Buy Price of ' +Contents[i]['currency']  + ' : '+ Contents[i]['avg_buy_price'] + Contents[i]['currency'])
-  total += float(Contents[i]['balance']) * float(Contents[i]['avg_buy_price'])
-
-print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
-print("안녕하세요 이상현 님")
-
-
-print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
-
-
+  Purchase_Total += float(Contents[i]['balance']) * float(Contents[i]['avg_buy_price'])
+  if (Contents[i]['currency'] != 'CHR'):
+    df = pyupbit.get_ohlcv("KRW-" + Contents[i]['currency'], interval="minute1", count=1)
+    Current_Total += float(Contents[i]['balance']) * float(df['open'][0])
+  else:
+    df = pyupbit.get_ohlcv("BTC-" + Contents[i]['currency'], interval="minute1", count=1)
+    Current_Total += float(Contents[i]['balance']) * float(df['open'][0]) 
 
 
 
-Property = format(round(total),',')
-
-print(str(Property) + "Won")  
+Purchase_Property = format(round(Purchase_Total),',')
+Current_Property = format(round(Current_Total),',')
+Fluctuation_Rate = (Current_Property / Purchase_Property) * 100 - 100
  
+
+print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+print("Hello! Lee! What can I do for you?/n/n/n/n")
+print("purchase price of all coins : " + str(Purchase_Property) + " Won")
+print("current price of all coins : "+ str(Current_Property) + ' Won')
+print("fluctuation rate : " + str(Fluctuation_Rate) +'%')
+
+print("[1] Check Property")
+print("[2] Buy")
+print("[3] Sell")
+
+
+print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+
+
+
+
+
