2c8eeae690657ec94a4ffdb6e99c4400bf5c3a03
LSH-SHL <60726651+LSH-SHL@users.noreply.github.com>
2022-01-08 13:12:37 +0900

    Update Load.py

--- Load.py
+++ Load.py
@@ -33,10 +33,10 @@ Fluctuation_Rate = (Current_Total / Purchase_Total) * 100 - 100
  
 
 print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
-print("Hello! Lee! What can I do for you?\n\n\n\n")
+print("Hello! Lee! What can I do for you?\n\n")
 print("purchase price of all coins : " + str(Purchase_Property) + " Won")
 print("current price of all coins : "+ str(Current_Property) + ' Won')
-print("\n\n\n\n")
+print("\n\n")
 print("fluctuation rate : " + str(round(Fluctuation_Rate)) +'%')
 print("[1] Check Property")
 print("[2] Buy")
@@ -48,13 +48,21 @@ if (Choice == 1):
   for i in range(1, len(Contents)):
     if (Contents[i]['currency'] == 'CHR'):
       df = pyupbit.get_ohlcv("BTC-" + Contents[i]['currency'], interval="minute1", count=1)
+      df2 = pyupbit.get_ohlcv("KRW-BTC", interval="minute1", count=1)
+      print("Currency : " + Contents[i]['currency'])
+      print("Purchase Price : " + Contents[i]['avg_buy_price'])
+      temp = df['open'][0] * df2['open'][0]
+      print("Current Price : " + str(temp))
+      Fluctuation_Rate_of_Coin = (temp / float(Contents[i]['avg_buy_price'])) * 100 - 100
+      print("fluctuation rate : " + str(round(Fluctuation_Rate_of_Coin)) +'%')
     else:
       df = pyupbit.get_ohlcv("KRW-" + Contents[i]['currency'], interval="minute1", count=1)
-    print("Currency : " + Contents[i]['currency'])
-    print("Purchase Price : " + Contents[i]['avg_buy_price'])
-    print("Current Price : " + str(df['open'][0]))
-    Fluctuation_Rate_of_Coin = (float(df['open'][0]) / float(Contents[i]['avg_buy_price'])) * 100 - 100
-    print("fluctuation rate : " + str(round(Fluctuation_Rate_of_Coin)) +'%')
+      print("Currency : " + Contents[i]['currency'])
+      print("Purchase Price : " + Contents[i]['avg_buy_price'])
+      print("Current Price : " + str(df['open'][0]))
+      Fluctuation_Rate_of_Coin = (float(df['open'][0]) / float(Contents[i]['avg_buy_price'])) * 100 - 100
+      print("fluctuation rate : " + str(round(Fluctuation_Rate_of_Coin)) +'%')
+    
     print("\n")
 
 
